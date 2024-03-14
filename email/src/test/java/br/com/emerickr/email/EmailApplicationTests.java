b83a7b935a7ac40ace2f2218323503f4817b64a6
Ranon Emerick <ranonemerick@gmail.com>
2024-03-14 11:40:59 -0300

    Iniciando o projeto

---
+++ email/src/test/java/br/com/emerickr/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emerickr.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
